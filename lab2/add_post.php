a1ebb98b707b0b4c59a9c941af287c5134f22509
Norman Zhu <nzhu@college.harvard.edu>
2012-03-07 10:27:28 -0500

    comments work

--- lab2/add_post.php
+++ lab2/add_post.php
@@ -4,11 +4,12 @@
 	require("common.php");
 
 	
-	// TODO 2: Check for presence of input parameters else die("warning")
-	if (empty($_POST["title"]) || empty($_POST["author"]) || 
-	    empty($_POST["URL"]))
-	    die("warning");
-	
+	// TODO 2: Check for presence of input parameters else alert
+	if (empty($_POST["title"]) || empty($_POST["author"]) || empty($_POST["URL"]))
+	{    print("Please fill out all fields");
+	}
+	else
+	{
 	// TODO 2: Save the cleaned input data
 	$title = mysql_real_escape_string($_POST["title"]);
 	$author = mysql_real_escape_string($_POST["author"]);
@@ -22,10 +23,10 @@
 	
 	mysql_query("INSERT INTO Books (title, author, url) VALUES ('$title', '$author',  '$url')");
 	
-	$result3 = mysql_query("SELECT * FROM Books WHERE title = $title");
+	$result3 = mysql_query("SELECT * FROM Books WHERE title = '$title'");
 	$row3 = mysql_fetch_array($result3);
 
 	print ($row3["id"]);
-
+	}
 	
 ?>
